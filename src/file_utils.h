370654936c3f9e222984b490f018ab93dcd4908e
Oleg Kutkov <elenbert@gmail.com>
2017-10-23 11:46:01 +0300

    fixed file utils new function get_file_size

--- src/file_utils.h
+++ src/file_utils.h
@@ -3,9 +3,10 @@
 
 typedef struct file_info {
 	char file_vendor[25];
-	size_t file_size;
+	long file_size;
 	uint8_t file_supported;
 } file_info_t;
 
 void get_file_info(char *fname, file_info_t *finf);
+long get_file_size(char *fname);
 
