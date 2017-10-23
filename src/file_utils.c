3ba5f44156b2106776c6f5a85b1e88244a825045
Oleg Kutkov <elenbert@gmail.com>
2017-10-23 11:46:01 +0300

    fixed file utils new function get_file_size
    Former-commit-id: 4d72a89328a4a34def0887bf321e22cacd63f8a3 [formerly 370654936c3f9e222984b490f018ab93dcd4908e]
    Former-commit-id: 4c47238082131c3c2bfb4dba1840558251e3dd22

--- src/file_utils.c
+++ src/file_utils.c
@@ -1,6 +1,8 @@
 
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include "file_utils.h"
 
 struct file_vendors {
@@ -43,7 +45,7 @@ void get_file_info(char *fname, file_info_t *finf)
 {
 	size_t fname_len = strlen(fname);
 	size_t il, vc;
-	char *lowcase_buf = (char *) malloc (fname_len);
+	char *lowcase_buf = (char *) malloc (fname_len + 1);
 
 	finf->file_supported = 0;
 
@@ -51,22 +53,36 @@ void get_file_info(char *fname, file_info_t *finf)
 		lowcase_buf[il] = tolower(fname[il]);
 	}
 
-	lowcase_buf[il] = '\0';
+	lowcase_buf[fname_len] = '\0';
 
 	char *extdot = strrchr(lowcase_buf, '.');
 
 	if (extdot == NULL) {
+		free(lowcase_buf);
 		return;
 	}
 
 	char *file_extension = extdot + 1;
 
 	for (vc = 0; vc < 9; vc++) {
-		if (strstr(file_extension, all_vendors[0]) != NULL) {
-			printf("%s\n", all_vendors[1]);
+		if (strstr(file_extension, all_vendors[vc].extensions) != NULL) {
+			finf->file_supported = 1;
+			strcpy(finf->file_vendor, all_vendors[vc].vendor);
+			finf->file_size = get_file_size(fname);
 		}
 	}
 
 	free(lowcase_buf);
+}
+
+long get_file_size(char *fname)
+{
+	struct stat statbuf;
+
+	if (stat(fname, &statbuf) < 0) {
+		return 0;
+	}
+
+	return statbuf.st_size;
 }
 
