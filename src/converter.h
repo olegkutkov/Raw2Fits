26eb9c27bf15be2efd2033067991fb843308d49f
Oleg Kutkov <elenbert@gmail.com>
2017-10-25 17:01:53 +0300

    include guards were added
    Former-commit-id: 087f910de82885f005b017b797627f8afb16b5ab [formerly 4b29def8bb927ee9a6de3291b41e02185c101e5b]
    Former-commit-id: d4401456a7245c57051fb9d9e2997a8a29f482e5

--- src/converter.h
+++ src/converter.h
@@ -1,9 +1,12 @@
 ////
 
+#ifndef __CONVERTER_H__
+#define __CONVERTER_H__
+
 #include "converter_types.h"
 
 void convert_files(converter_params_t *params);
 void converter_stop(converter_params_t *params);
 
-//void convert_files(char *inpath, char *outpath);
+#endif
 
--- src/thread_pool.h
+++ src/thread_pool.h
@@ -1,8 +1,13 @@
 
+#ifndef __THREAD_POOL_H__
+#define __THREAD_POOL_H__
+
 typedef void* (*thread_task) (void *arg);
 
 void init_thread_pool(size_t num_threads);
 void thread_pool_add_task(thread_task task, void *task_arg);
 void thread_pool_stop_tasks();
 void cleanup_thread_pool();
+
+#endif
 
