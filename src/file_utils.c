af357d9f7f2380dc0891e395513e9ba2ec7c61b0
Oleg Kutkov <elenbert@gmail.com>
2017-10-26 01:38:46 +0300

    fits filename builder was added

--- src/file_utils.c
+++ src/file_utils.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <libgen.h>
 #include "file_utils.h"
 
 struct file_vendors {
@@ -94,5 +95,97 @@ int is_file_exist(char *filename)
 	return (stat (filename, &buffer) == 0);
 }
 
+void make_target_fits_filename(converter_params_t *arg, char *raw_filename, char *out_filename)
+{
+	int iter = 0;
+	char *out_file_name_base, *obj, *datetime, *filter;
+	size_t outdir_len = strlen(arg->outpath);
+	char *base_raw_filename = basename(raw_filename);
+	size_t raw_filename_len = strlen(base_raw_filename);
+	size_t obj_len = 0, datetime_len = 0, filter_len = 0;
+
+	strncpy(out_filename, arg->outpath, outdir_len);
+	out_filename[outdir_len] = '/';
+
+	switch (arg->fsetup.naming) {
+		case RAW_NAME:
+			out_file_name_base = (char *) malloc(raw_filename_len);
+			strncpy(out_file_name_base, base_raw_filename, raw_filename_len - 3);
+			out_file_name_base[raw_filename_len - 3] = '\0';
+
+			break;
+
+		case OBJECT_DATETIME:
+			if (strlen(arg->meta.object) > 0) {
+				obj = arg->meta.object;
+			} else {
+				obj = base_raw_filename;
+			}
+
+			if (strlen(arg->meta.date) > 0) {
+				datetime = arg->meta.date;
+			} else {
+				datetime = "NO_DATE";
+			}
+
+			obj_len = strlen(obj);
+			datetime_len = strlen(datetime);
+
+			out_file_name_base = (char *) malloc(obj_len + datetime_len + 4);
+			strcpy(out_file_name_base, obj);
+			strncpy(out_file_name_base + obj_len, "_", 1);
+			strcpy(out_file_name_base + obj_len + 1, datetime);
+
+			raw_filename_len = obj_len + datetime_len + 5;
+
+			break;
+
+		case OBJECT_FILTER_DATETIME:
+			if (strlen(arg->meta.object) > 0) {
+				obj = arg->meta.object;
+			} else {
+				obj = base_raw_filename;
+			}
+
+			if (strlen(arg->meta.filter) > 0) {
+				filter = arg->meta.filter;
+			} else {
+				filter = "NO_FILTER";
+			}
+
+			if (strlen(arg->meta.date) > 0) {
+				datetime = arg->meta.date;
+			} else {
+				datetime = "NO_DATE";
+			}
+
+			obj_len = strlen(obj);
+			filter_len = strlen(filter);
+			datetime_len = strlen(datetime);
+
+			out_file_name_base = (char *) malloc(obj_len + filter_len + datetime_len + 5);
+			strcpy(out_file_name_base, obj);
+			strncpy(out_file_name_base + obj_len, "_", 1);
+			strncpy(out_file_name_base + obj_len + 1, filter, filter_len);
+			strncpy(out_file_name_base + obj_len + filter_len + 1, "_", 1);
+			strcpy(out_file_name_base + obj_len + filter_len + 2, datetime);
+
+			raw_filename_len = obj_len + filter_len + datetime_len + 6;
+
+			break;
+	}
+
+	strncpy(out_filename + outdir_len + 1, out_file_name_base, raw_filename_len);
+	strncpy(out_filename + outdir_len + 1 + raw_filename_len - 4, ".fits", 6);
+	out_filename[outdir_len + raw_filename_len + 2] = '\0';
+
+	for (; iter < strlen(out_filename); ++iter) {
+		if (out_filename[iter] == 0x20) {
+			out_filename[iter] = '_';
+		}
+	}
+
+	free(out_file_name_base);
+}
 
 
