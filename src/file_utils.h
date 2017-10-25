8e0b87c1365e7ba1e60c8588e254e23dc758803c
Oleg Kutkov <elenbert@gmail.com>
2017-10-26 01:38:46 +0300

    fits filename builder was added
    Former-commit-id: 72507066b07715254218ffbd9d0f909b5e39bd8f [formerly af357d9f7f2380dc0891e395513e9ba2ec7c61b0]
    Former-commit-id: e0f63c288a7e3ed135b2edc27ac770447e064045

--- src/file_utils.h
+++ src/file_utils.h
@@ -3,6 +3,7 @@
 #define __FILE_UTILS_H__
 
 #include <stdint.h>
+#include "converter_types.h"
 
 typedef struct file_info {
 	char file_vendor[25];
@@ -13,6 +14,8 @@ typedef struct file_info {
 void get_file_info(char *fname, file_info_t *finf);
 long get_file_size(char *fname);
 int is_file_exist(char *filename);
+
+void make_target_fits_filename(converter_params_t *arg, char *raw_filename, char *out_filename);
 
 #endif
 
