3a8a649ce8f1547a64dda0757c1d9f14a3165e08
Oleg Kutkov <elenbert@gmail.com>
2017-10-28 01:10:44 +0300

    correctly reinitialize thread pool critical section routines was added

--- src/thread_pool.h
+++ src/thread_pool.h
@@ -6,8 +6,10 @@ typedef void* (*thread_task) (void *arg);
 
 void init_thread_pool(size_t num_threads);
 void thread_pool_add_task(thread_task task, void *task_arg);
-void thread_pool_stop_tasks();
 void cleanup_thread_pool();
+
+void task_enter_critical_section();
+void task_exit_critical_section();
 
 #endif
 
