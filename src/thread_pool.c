1e7185d54286bdace922da1e52d83b428e3f9d6c
Oleg Kutkov <elenbert@gmail.com>
2017-10-28 01:10:44 +0300

    correctly reinitialize thread pool critical section routines was added
    Former-commit-id: d629557771722c8f3b269fad73e959a7231e990b [formerly 3a8a649ce8f1547a64dda0757c1d9f14a3165e08]
    Former-commit-id: 2d0dfbb77dd488c999fa118e5cefdfe292c226a9

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -7,11 +7,15 @@ static pthread_t *threads = NULL;
 static int total_threads = 0;
 static int threads_pool_cursor = 0;
 
+static pthread_mutex_t pool_lock;
+
 void init_thread_pool(size_t num_threads)
 {
 	if (threads) {
-		free(threads);
+		cleanup_thread_pool();
 	}
+
+	pthread_mutex_init(&pool_lock, NULL);
 
 	threads = (pthread_t*) malloc (sizeof(pthread_t) * num_threads);
 	total_threads = num_threads;
@@ -25,17 +29,14 @@ void thread_pool_add_task(thread_task task, void *task_arg)
 	threads_pool_cursor++;
 }
 
-void thread_pool_stop_tasks()
+void cleanup_thread_pool()
 {
 	int i;
 
 	for (i = 0; i < total_threads; i++) {
 		pthread_join(threads[i], NULL);
 	}
-}
 
-void cleanup_thread_pool()
-{
 	if (threads) {
 		free(threads);
 		threads = NULL;
@@ -43,5 +44,17 @@ void cleanup_thread_pool()
 
 	total_threads = 0;
 	threads_pool_cursor = 0;
+
+	pthread_mutex_destroy(&pool_lock);
+}
+
+void task_enter_critical_section()
+{
+	pthread_mutex_lock(&pool_lock);
+}
+
+void task_exit_critical_section()
+{
+	pthread_mutex_unlock(&pool_lock);
 }
 
