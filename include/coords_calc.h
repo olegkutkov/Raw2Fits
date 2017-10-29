e6f54834960bec142e382d5eb1f0b94d9b841137
Oleg Kutkov <elenbert@gmail.com>
2017-10-30 02:51:07 +0300

    auxilarity functions for coords calculations
    Former-commit-id: aa50b802459d6408cfcd087935755369f2679f28 [formerly 48ff64db128692f7612fdc62bfccf44ab986f163]
    Former-commit-id: 0276656e94e5c3a4e332cf7c46c684dc264ae6d7

---
+++ include/coords_calc.h
@@ -0,0 +1,26 @@
+/* 
+   coords_calc.h
+
+   Copyright 2017  Oleg Kutkov <elenbert@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ */
+
+#ifndef __COORDS_CALC_H__
+#define __COORDS_CALC_H__
+
+float coord_any_to_float_deg(const char *str);
+
+#endif
+
