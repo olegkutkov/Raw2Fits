6bef2648cf751a843cc98584963c427a25eb8b59
Oleg Kutkov <elenbert@gmail.com>
2017-11-01 02:31:31 +0300

    header with version info
    Former-commit-id: 9c3daba1472d4fdda67fda0ed4a5b0e793c8ca6e [formerly 2d99a9222b6c4a2f422af7359b52d6d309a5399a]
    Former-commit-id: 0377224ca31b3cbdedb3de54fddcd5ebb8591c2f

---
+++ include/version.h
@@ -0,0 +1,28 @@
+/* 
+   version.h
+
+   Copyright 2017  Oleg Kutkov <elenbert@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ */
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define RAW2FITS_VERSION_MAJOR 0
+#define RAW2FITS_VERSION_MINOR 3
+#define RAW2FITS_VERSION_PATCH 0
+
+#endif
+
