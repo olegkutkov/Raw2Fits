10f5a79ffd7459aa2b2c0876d2e307351d4bff83
Oleg Kutkov <elenbert@gmail.com>
2017-11-07 15:41:36 +0300

    cli configuration file parser in separate file
    Former-commit-id: e30ebbca2ee732dded3b871580ec361d53add789 [formerly 4a6bdabb9fbc395ed0f0c3ace711d340eb3027d1]
    Former-commit-id: 65ea3147511b89d118fe53e11c9486c52445e8ac

---
+++ include/config_loader.h
@@ -0,0 +1,30 @@
+/* 
+  config_loader.h
+
+   Copyright 2017  Oleg Kutkov <elenbert@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ */
+
+#ifndef __CONFIG_LOADER_H__
+#define __CONFIG_LOADER_H__
+
+#include "converter_types.h"
+
+int load_configuration(char *confile, converter_params_t *conv_params);
+int validate_configuration(converter_params_t *conv_params);
+void dump_configuration(converter_params_t *conv_params);
+
+#endif
+
